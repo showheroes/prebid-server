2ef75f79a405f36869dcc90149fda5ba6d48c258
Filip Stamenkovic <filip.stamenkovic@showheroes-group.com>
2026-05-26 11:58:40 +0200

    make reviewers happy

--- openrtb_ext/imp_genericvast.go
+++ openrtb_ext/imp_genericvast.go
@@ -2,6 +2,6 @@ package openrtb_ext
 
 // ExtImpGenericVast defines the contract for bidrequest.imp[i].ext.prebid.bidder.genericvast.
 type ExtImpGenericVast struct {
-	URL string   `json:"url"`
-	CPM *float64 `json:"cpm,omitempty"`
+	URL string  `json:"url"`
+	CPM float64 `json:"cpm"`
 }
